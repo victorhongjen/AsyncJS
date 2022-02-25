4b5695ff62fa7a6a7251442dd966c1bc3643a9a0
Victor Wang <hongjenwang@gmail.com>
2022-02-24 21:27:47 -0800

    Add condition to check request status

--- sandbox.js
+++ sandbox.js
@@ -2,9 +2,11 @@ const request = new XMLHttpRequest();
 
 request.addEventListener("readystatechange", () => { //fired when state changes
     //console.log(request, request.readyState)// request obj and its current state
-    if (request.readyState === 4) {
+    if (request.readyState === 4 && request.status === 200) { 
         //do something to the data
         console.log(request.responseText) // contains response data
+    } else if (request.readyState === 4) {
+        console.log("couldn't fetch data ");
     }
 })
 
