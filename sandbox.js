8083250dd1f50468f362ce5ff614aba97c798f98
Victor Wang <hongjenwang@gmail.com>
2022-02-24 22:20:33 -0800

    Add async callback function

--- sandbox.js
+++ sandbox.js
@@ -1,18 +1,34 @@
-const request = new XMLHttpRequest();
+const getToDos = (callback) => { 
+    const request = new XMLHttpRequest();
+    
+    request.addEventListener("readystatechange", () => { //fired when state changes
+        //console.log(request, request.readyState)// request obj and its current state
+        if (request.readyState === 4 && request.status === 200) { 
+            //do something to the  data
+            callback(undefined, request.responseText) //callback function with error or responseText
+        } else if (request.readyState === 4) {
+            callback("couldn't fetch data", undefined)
+        }
+    })
+    
+    request.open("GET", "https://jsonplaceholder.typicode.com/todos/"); //setting up request path
+    
+    request.send();
+};
 
-request.addEventListener("readystatechange", () => { //fired when state changes
-    //console.log(request, request.readyState)// request obj and its current state
-    if (request.readyState === 4 && request.status === 200) { 
-        //do something to the data
-        console.log(request.responseText) // contains response data
-    } else if (request.readyState === 4) {
-        console.log("couldn't fetch data ");
+console.log(1)
+console.log(2)
+getToDos((err, data) => { //add callback function in getToDos function. Convention is err, data.
+    console.log("callback fired"); //will not block the code. Pass back data when network data request complete
+    if(err) {
+        console.log(err);
+    }else {
+        console.log(data);
     }
-})
+}); 
+console.log(3)
+console.log(4)
 
-request.open("GET", "https://jsonplaceholder.typicode.com/todos/"); //setting up request path
-
-request.send();
 
 
 
